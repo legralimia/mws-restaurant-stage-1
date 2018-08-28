49ec0c1fb252e452cb3b06bb26c3c991291c8e14
legralimia <gladys.legra@gmail.com>
2018-08-27 23:02:21 -0400

    Fix:change service worker to the root

--- js/register.js
+++ js/register.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('js/sw.js')
+    navigator.serviceWorker.register('/sw.js')
     .then(reg=>{
         console.log("Service worker registration succesfull. "+reg.scope);
     }).catch(error=>{
